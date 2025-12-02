a4771429c5efeca5088e09e117560165455c5ac8
reinanagoshi <reina.nagoshi@benilde.edu.ph>
2025-12-02 10:56:04 +0800

    Undo

---
+++ QUIZ_4/src/StoredPlayer.java
@@ -0,0 +1,18 @@
+public class StoredPlayer
+{
+    public String key;
+    public Player value;
+
+    public StoredPlayer(String key, Player value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+
+    public String toString()
+    {
+        return key + " => " + value;
+    }
+}
